770eb4541462dd4aeb267149b850339de8b3815c
Andrew Kane <andrew@ankane.org>
2024-08-28 11:09:36 -0700

    Updated model to use embedding pipeline [skip ci]

--- lib/informers/model.rb
+++ lib/informers/model.rb
@@ -2,7 +2,7 @@ module Informers
   class Model
     def initialize(model_id, quantized: false)
       @model_id = model_id
-      @model = Informers.pipeline("feature-extraction", model_id, quantized: quantized)
+      @model = Informers.pipeline("embedding", model_id, quantized: quantized)
     end
 
     def embed(texts)
@@ -11,9 +11,9 @@ module Informers
 
       case @model_id
       when "sentence-transformers/all-MiniLM-L6-v2", "Xenova/all-MiniLM-L6-v2", "Xenova/multi-qa-MiniLM-L6-cos-v1", "Supabase/gte-small"
-        output = @model.(texts, pooling: "mean", normalize: true)
+        output = @model.(texts)
       when "mixedbread-ai/mxbai-embed-large-v1"
-        output = @model.(texts, pooling: "cls")
+        output = @model.(texts, pooling: "cls", normalize: false)
       else
         raise Error, "model not supported: #{@model_id}"
       end
