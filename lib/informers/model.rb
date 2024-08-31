e5b3b7f7ccc92040eee4d8b6276014fa66840ea0
Andrew Kane <andrew@ankane.org>
2024-08-31 15:02:32 -0700

    Simplified code [skip ci]

--- lib/informers/model.rb
+++ lib/informers/model.rb
@@ -1,19 +1,12 @@
 module Informers
   class Model
     def initialize(model_id, quantized: false)
-      @model_id = model_id
       @model = Informers.pipeline("embedding", model_id, quantized: quantized)
+      @options = model_id == "mixedbread-ai/mxbai-embed-large-v1" ? {pooling: "cls", normalize: false} : {}
     end
 
     def embed(texts)
-      case @model_id
-      when "sentence-transformers/all-MiniLM-L6-v2", "Xenova/all-MiniLM-L6-v2", "Xenova/multi-qa-MiniLM-L6-cos-v1", "Supabase/gte-small"
-        @model.(texts)
-      when "mixedbread-ai/mxbai-embed-large-v1"
-        @model.(texts, pooling: "cls", normalize: false)
-      else
-        raise Error, "Use the embedding pipeline for this model: #{@model_id}"
-      end
+      @model.(texts, **@options)
     end
   end
 end
